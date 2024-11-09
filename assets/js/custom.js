ae85830d9ac890ab3e96c403b2e7cafbd13df48f
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-11-09 10:50:13 +0100

    Almost there i think!

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,6 +11,18 @@
         if (iframe && iframe.contentWindow) {
             iframe.contentWindow.postMessage({ theme: currentTheme }, '*');
         }
+
+        // Dummy test
+        function updateTerminalColor(row, color) {
+            document.documentElement.style.setProperty(`--terminal-r${row}-fill`, color);
+        }
+
+        if (currentTheme == "light"){
+            updateTerminalColor(1, '#ff0000'); // Changes r1 to red
+        } else {
+            updateTerminalColor(1, '#00ff00'); // Changes r3 to green
+        }
+
         console.log("ZZZ")
     }
 
